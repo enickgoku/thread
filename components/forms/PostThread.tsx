6d776837d3cf2eeb78eaa55a1d5a1962a2a1c252
Nick Engelhardt <enickgoku2@gmail.com>
2023-08-12 15:32:33 -0400

    started acting post thread

---
+++ components/forms/PostThread.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const PostThread = ({ userId }: { userId: string }) => {
+  return (
+    <>
+      <h1>Post Thread</h1>
+      <p>Ahhhhhh</p>
+      <p>{userId}</p>
+    </>
+  );
+};
+
+export default PostThread;
