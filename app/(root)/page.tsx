ace008cb058fd30182dbec56e468a63b42ee5408
Nick Engelhardt <enickgoku2@gmail.com>
2023-08-16 14:51:32 -0400

    working on post card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,36 @@
 import { fetchPosts } from "@/lib/actions/thread.actions";
+import { currentUser } from "@clerk/nextjs";
+
+import ThreadCard from "@/components/cards/ThreadCard";
 
 export default async function Home() {
   const result = await fetchPosts(1, 30);
-
-  console.log(result);
+  const user = await currentUser();
 
   return (
     <div>
       <h1 className="head-text text-left">Threads for music people only</h1>
+      <section className="mt-9 flex flex-col gap-10">
+        {result.posts.length === 0 ? (
+          <p className="no-result">No Posts yet!</p>
+        ) : (
+          <>
+            {result.posts.map((post) => (
+              <ThreadCard
+                key={post.id}
+                id={post.id}
+                currentUserId={user?.id || ""}
+                parentId={post.parentId}
+                content={post.text}
+                author={post.author}
+                community={post.community}
+                createdAt={post.createdAt}
+                comments={post.children}
+              />
+            ))}
+          </>
+        )}
+      </section>
     </div>
   );
 }
